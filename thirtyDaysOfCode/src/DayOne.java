fcbbcaf9bb122ec44b9278b70687272474302cf9
Mohamed Faisal <mohamedfaizuu@gmail.com>
2023-04-13 01:39:56 +0530

    Initial Commit

---
+++ thirtyDaysOfCode/src/DayOne.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class DayOne {
+    public static void main(String[] args) {
+int i=4;
+double d=4.0;
+String s="HackerRank ";
+Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        double n = in.nextDouble();
+        in.nextLine();
+        String o = in.nextLine();
+
+        System.out.println(i + m);
+        System.out.println(d + n);
+        System.out.println(s + o);
+    }
+}
