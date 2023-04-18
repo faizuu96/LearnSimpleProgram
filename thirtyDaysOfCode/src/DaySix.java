2234db9ec210de8e006962249d800fdd8c463331
Mohamed Faisal <mohamedfaizuu@gmail.com>
2023-04-18 16:05:46 +0530

    Initial Commit

---
+++ thirtyDaysOfCode/src/DaySix.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DaySix {
+    //input: abcd
+    //output:ac bd
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int numString = in.nextInt();
+    int i,j;
+for(i=0;i<numString;i++){
+    String s = in.next();
+    String even = "";
+    String odd = "";
+    for(j=0;j<s.length();j++){
+        if(j%2==0){
+            even += s.charAt(j);
+        }else{
+            odd += s.charAt(j);
+        }
+    }
+    System.out.println(even + " " + odd);
+}
+
+    }
+}
+
