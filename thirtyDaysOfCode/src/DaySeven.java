388e982f9913e62c6a95a6b0a0b333467d341ae9
Mohamed Faisal <97228128+faizuu96@users.noreply.github.com>
2023-04-20 13:17:47 +0530

    Update DaySeven.java

--- thirtyDaysOfCode/src/DaySeven.java
+++ thirtyDaysOfCode/src/DaySeven.java
@@ -11,21 +11,18 @@ Scanner in = new Scanner(System.in);
  int n = in.nextInt();
  int[] ar;//declare
  ar=new int[n];//allocate
-for(int i=0;i<n;i++){
+
+        for(int i=0;i<n;i++){
     ar[i] = in.nextInt();//initialize a array input
-}for(int i=0;i<n/2;i++){
+}
+        for(int i=0;i<n/2;i++){
     int temp = ar[i];
     ar[i]= ar[n-1-i];
     ar[n-1-i] = temp;
         }
-System.out.print("[");
         for(int i=0;i<n;i++){
             System.out.print(ar[i]);
-            if(i<n-1){
-                System.out.print(", ");
-            }
-    }
-        System.out.print("]");
+        }
     }
 }
 
