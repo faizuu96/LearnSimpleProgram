f80694629342549b6ebb1274da7612c76dc78220
Mohamed Faisal <mohamedfaizuu@gmail.com>
2023-04-20 15:29:10 +0530

    Initial Commit

--- thirtyDaysOfCode/src/DayEight.java
+++ thirtyDaysOfCode/src/DayEight.java
@@ -1,19 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class DayEight {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        for(int i=0; i<n;i++){
-            String name = in.next();
-            int phone = in.nextInt();
 
-        while(in.hasNext()) {
-            String s = in.next();
-            if(name==s){
-                System.out.println(name+"="+phone);
-            }
+        /*input
+3
+sam 99912222
+tom 11122222
+harry 12299933
+sam
+edward
+harry
+
+output
+sam=99912222
+Not found
+harry=12299933
+        */
+        public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number of custoer details in phone book:");
+        int n = in.nextInt();
+
+        Map<String, Integer> phoneBook = new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            String name1 = in.next();
+            int number1 = in.nextInt();
+            phoneBook.put(name1,number1);
         }
+        while (in.hasNext()){
+            String name2 = in.next();
+            Integer number2 = phoneBook.get(name2);
+            if(number2 != null){
+                System.out.println(name2+"="+number2);
+            }else{
+                System.out.println("Not found");
+            }
         }
         in.close();
     }
