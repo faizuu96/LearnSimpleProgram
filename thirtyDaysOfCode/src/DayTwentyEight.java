ec602b596be6381b9804c6acfe04800a44897a43
Mohamed Faisal <mohamedfaizuu@gmail.com>
2023-05-08 23:34:24 +0530

    DayTwentyEight

---
+++ thirtyDaysOfCode/src/DayTwentyEight.java
@@ -0,0 +1,19 @@
+
+    import java.util.Scanner;
+
+    public class DayTwentyEight {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            String s = scan.next();
+            scan.close();
+
+            try {
+                int num = Integer.parseInt(s);
+                System.out.println(num);
+            } catch (NumberFormatException e) {
+                System.out.println("Bad String");
+            }
+        }
+    }
+
+
