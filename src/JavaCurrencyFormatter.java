3b5c73608d188924f13eccdad00331b8f71e2b54
Mohamed Faisal <mohamedfaizuu@gmail.com>
2023-05-29 13:21:18 +0530

    Md_S_Things

---
+++ src/JavaCurrencyFormatter.java
@@ -0,0 +1,40 @@
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class JavaCurrencyFormatter {
+   // Input/Output
+   /* 12324.134
+    US: $12,324.13
+    India: Rs.12,324.13
+    China: ￥12,324.13
+    France: 12 324,13 €*/
+   public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       double value = in.nextDouble();
+       in.close();
+
+       NumberFormat usFormat = NumberFormat.getCurrencyInstance(Locale.US);
+       String usCurrency = usFormat.format(value);
+
+       DecimalFormat indFormat = new DecimalFormat("'Rs.'###,###.00");
+       String indCurrency = indFormat.format(value);
+
+       DecimalFormat chinaFormat = new DecimalFormat("¥###,###.00");
+       String chinaCurrency = chinaFormat.format(value);
+
+       NumberFormat franceFormat = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+       String franceCurrency = franceFormat.format(value);
+
+       System.out.println("US: " + usCurrency);
+       System.out.println("India: " + indCurrency);
+       System.out.println("China: " + chinaCurrency);
+       System.out.println("France: " + franceCurrency);
+
+
+   }
+
+
+}
