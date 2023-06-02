48fc269e6be39da6f79fff289a9ffb6f2d5ca4fa
Mohamed Faisal <mohamedfaizuu@gmail.com>
2023-06-02 12:40:14 +0530

    Md_S_Things

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
-        String original,reverse="";
+        /*String original,reverse="";
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the String");
         original = s.nextLine();
@@ -13,10 +13,26 @@ public class Palindrome {
             if(original.equals(reverse)){
                 System.out.println("Enter the value is palindrome");
             }else{
-                System.out.println("Enter the value is not palindrome");
+                System.out.println("Enter the value is not palindrome");*/
+
+        Scanner in = new Scanner(System.in);
+        String A = in.next();
+        int l = A.length();
+        boolean isPalindrome = true;
+        for(int i=l-1;i>=0;i--) {
+
+            char r = A.charAt(i);
+            if (r != A.charAt(l - 1 - i)) {
+                isPalindrome = false;
+                break;
             }
         }
-
-
+        if(isPalindrome){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+        in.close();
+            }
 
 }
