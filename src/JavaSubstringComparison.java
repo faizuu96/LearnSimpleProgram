4f86c984a61b25e70ce0a029b5a43ec85495afe0
Mohamed Faisal <mohamedfaizuu@gmail.com>
2023-06-04 23:58:39 +0530

    Md_S_Things

--- src/JavaSubstringComparison.java
+++ src/JavaSubstringComparison.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class JavaSubstringComparison {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         String min="";
         String max="";
         Scanner scan = new Scanner(System.in);
@@ -31,5 +31,39 @@ public class JavaSubstringComparison {
         System.out.print(min+"\n"+max);
 
 
+    }*/
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+
+        // Initialize smallest and largest with the first substring of length 'k'
+        smallest = s.substring(0, k);
+        largest = s.substring(0, k);
+
+        // Compare all the remaining substrings of length 'k'
+        for (int i = 1; i <= s.length() - k; i++) {
+            String substr = s.substring(i, i + k);
+
+            // Compare substr with the current smallest and largest substrings
+            if (substr.compareTo(smallest) < 0) {
+                smallest = substr; // Update smallest if substr is lexicographically smaller
+            }
+            if (substr.compareTo(largest) > 0) {
+                largest = substr; // Update largest if substr is lexicographically larger
+            }
+        }
+
+        return smallest + "\n" + largest;
     }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+
 }
