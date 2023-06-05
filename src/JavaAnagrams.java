4bed902ff6440fa86f02d4d8fe14130a74e783a9
Mohamed Faisal <mohamedfaizuu@gmail.com>
2023-06-05 16:14:34 +0530

    Md_S_Things

--- src/JavaAnagrams.java
+++ src/JavaAnagrams.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class JavaAnagrams {
 
 
-        static boolean isAnagram(String a, String b) {
+       /* static boolean isAnagram(String a, String b) {
             // Complete the function
             a = a.toLowerCase();
             b = b.toLowerCase();
@@ -33,4 +33,48 @@ public class JavaAnagrams {
         }
 
 
-}
+}*/
+
+
+        static boolean isAnagram(String a, String b) {
+            // Complete the function
+            a = a.toLowerCase();
+            b = b.toLowerCase();
+
+            if(a.length() != b.length()){
+                return false;
+            }
+
+            int[] freqA = new int[26];
+            int[] freqB = new int[26];
+
+            // Populate frequency array for string A
+            for (char c : a.toCharArray()) {
+                freqA[c - 'a']++;
+            }
+
+            // Populate frequency array for string B
+            for (char c : b.toCharArray()) {
+                freqB[c - 'a']++;
+            }
+
+            // Compare the frequency arrays
+            for (int i = 0; i < 26; i++) {
+                if (freqA[i] != freqB[i]) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        public static void main(String[] args) {
+
+            Scanner scan = new Scanner(System.in);
+            String a = scan.next();
+            String b = scan.next();
+            scan.close();
+            boolean ret = isAnagram(a, b);
+            System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+        }
+    }
+
