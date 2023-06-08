4f8887eeb4052ec82bb192fe471ce621d4ca0889
Mohamed Faisal <mohamedfaizuu@gmail.com>
2023-06-08 10:25:23 +0530

    Md_S_Things

--- guviBasic/src/Program9.java
+++ guviBasic/src/Program9.java
@@ -1,6 +1,12 @@
 import java.util.*;
-
-public class FindDuplicate {
+      /*Given 2 numbers N and K followed by N elements,print the number of repetition of K otherwise print '-1' if the element not found.
+        Sample Testcase :
+        INPUT
+        6 2
+        1 2 3 5 7 8
+        OUTPUT
+        0*/
+public class Program9 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
